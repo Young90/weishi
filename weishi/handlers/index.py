d79f77f175429d486d99a33902b57c28bb5d3e7c
Young <jackiehou@163.com>
2013-12-20 00:44:10 +0800

    ueditor

--- weishi/handlers/index.py
+++ weishi/handlers/index.py
@@ -8,11 +8,7 @@ from tornado import template
 
 class IndexHandler(RequestHandler):
     def get(self):
-        result = {'ToUserName': 'toUser', 'FromUserName': 'developer', 'CreateTime': int(time.time()),
-                  'Articles': [{}, {}], 'MsgType': 'text', 'Content': u'欢迎关注'}
-        self.write(template.Loader(self.get_template_path())
-        .load(name='text_message.xml', parent_path='message').generate(result=result))
-
+        self.render('account/article.html')
 
 handlers = [
     (r'/index/test', IndexHandler)
