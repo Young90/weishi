c32cee244320317588d32f8c74852de3062fafdc
Young <jackiehou@163.com>
2014-01-21 20:59:06 +0800

    account

--- weishi/libs/message.py
+++ weishi/libs/message.py
@@ -1,7 +1,11 @@
 #coding:utf-8
 __author__ = 'Young'
 
+import datetime
+
 from weishi import db
+from weishi.libs import wei_api
+
 
 db.connect()
 db = db.conn.mysql
@@ -11,10 +15,29 @@ def process_message(account, message):
     msg_type = message['MsgType'].lower()
     print msg_type
     if msg_type == 'text':
-        return _process_text_message(account, message)
+        return _process_text_message(account.aid, message)
+    if msg_type == 'image':
+        return _process_image_message(account.aid, message)
+    if msg_type == 'voice':
+        return _process_voice_message(account.aid, message)
+    if msg_type == 'video':
+        return _process_video_message(account.aid, message)
+    if msg_type == 'location':
+        return _process_location_message(account.aid, message)
+    if msg_type == 'link':
+        return _process_link_message(account.aid, message)
 
+
+def _add_single_fan(user, aid):
+    """将关注的用户保存到数据库"""
+    db.execute('insert into t_fans (date, openid, nickname, sex, country, province, city, avatar, '
+                    'subscribe_time, language, aid) values (NOW(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                    user['openid'], user['nickname'], user['sex'], user['country'], user['province'],
+                    user['city'], user['headimgurl'], datetime.datetime.fromtimestamp(int(user['subscribe_time'])),
+                    user['language'], aid)
 
 def _process_text_message(aid, message):
+
     """接受用户发送的文本消息"""
     openid = message['FromUserName']
     content = message['Content']
@@ -79,3 +102,13 @@ def _process_link_message(aid, message):
     url = message['Url']
     db.insert('insert into t_message (type, create_time, message_id, url, status, openid, aid) '
               'values (%s, %s, %s, %s, %s, %s, %s)', 'video', create_time, msg_id, url, 0, openid, aid)
+
+
+def _process_subscribe_event(account, message):
+    """
+    处理用户关注账号的事件
+        1. 将用户信息保存到数据库
+        2. 如果有自动回复消息，则回复
+    """
+    openid = message['FromUserName']
+    wei_api.get_user_info(account, openid, _add_single_fan)
